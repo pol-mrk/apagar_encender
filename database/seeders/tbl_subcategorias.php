9436cbe2ed4c0a46a8baeb5953cd7990c87adf0a
Julio-Cesar-Carrillo <100006703.joan23@fje.edu>
2024-03-07 09:51:51 +0100

    actualizar seeder

--- database/seeders/tbl_subcategorias.php
+++ database/seeders/tbl_subcategorias.php
@@ -18,8 +18,51 @@ class tbl_subcategorias extends Seeder
         $now = Carbon::now();
         DB::table("tbl_subcategorias")->insert([
             [
-                'nombre_sub_cat' => 'teclado',
+                'nombre_sub_cat' => 'Aplicació gestió administrativa',
                 'id_categoria' => 1,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'nombre_sub_cat' => 'Accés remot',
+                'id_categoria' => 1,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'nombre_sub_cat' => 'Aplicació de videoconferència',
+                'id_categoria' => 1,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'nombre_sub_cat' => 'Imatge de projector defectuosa',
+                'id_categoria' => 1,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+
+            [
+                'nombre_sub_cat' => 'Problema amb el teclat',
+                'id_categoria' => 2,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'nombre_sub_cat' => 'El ratolí no funciona',
+                'id_categoria' => 2,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'nombre_sub_cat' => "Monitor no s'encén",
+                'id_categoria' => 2,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'nombre_sub_cat' => 'Imatge de projector defectuosa',
+                'id_categoria' => 2,
                 'created_at' => $now,
                 'updated_at' => $now,
             ],

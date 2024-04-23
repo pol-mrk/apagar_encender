3b9b020d8f39ad0062f8dba657a3fa7551ee5aa1
Ian <100007039.joan23@fje.edu>
2024-04-23 20:38:57 +0200

    estilos

--- resources/views/crud_incidencias/ver.blade.php
+++ resources/views/crud_incidencias/ver.blade.php
@@ -1,50 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>CRUD php Vanilla</title>
-  <!-- Se ha de añadir el token para poder usarlo en el formdata de AJAX -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
-  <h1>DATOS</h1>
-<div class="container">
-
-</div>
-{{-- {{dd($incidencias)}} --}}
-      <!-- Segundo una tarjeta con los datos del CRUD a mostrar -->
-      {{-- <div class="row mt-3" id="resultado2"> --}}
-        @foreach ($incidencias as $incidencia)
-        <h5> Titulo: {{ $incidencia->titulo_inc }}</h5>
-        <p>{{ $incidencia->desc_inc }}</p>
-        <p>{{ $incidencia->created_at }}</p>
-        <p>{{ $incidencia->nombre_user }}</p>
-        <p>{{ $incidencia->nombre_sub_cat }}</p>
-        <p>{{ $incidencia->nombre_tecnico }}</p>
-        <p> {{ $incidencia->foto_inc }}</p>
-    @endforeach
-        <a href="/crud_incidencias" class="btn btn-primary">Volver</a>
-        <!-- Aquí se llenarán las tarjetas con los datos de incidencias -->
-     
-    
- 
-
-<!-- Enlazamos con el script de JS que se encargará de hacer las peticiones AJAX -->
-{{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-<script src="{{ asset('js/script2.js') }}"></script> --}}
+  <div class="container-fluid">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card mt-5">
+          <div class="card-header">
+            DATOS
+          </div>
+          <div class="card-body">
+            @foreach ($incidencias as $incidencia)
+            <h5 class="card-title">Titulo: {{ $incidencia->titulo_inc }}</h5>
+            <p class="card-text">{{ $incidencia->desc_inc }}</p>
+            <p>{{ $incidencia->created_at }}</p>
+            <p>{{ $incidencia->nombre_user }}</p>
+            <p>{{ $incidencia->nombre_sub_cat }}</p>
+            <p>{{ $incidencia->nombre_tecnico }}</p>
+            <p>{{ $incidencia->foto_inc }}</p>
+            @endforeach
+          </div>
+          <div class="card-footer">
+            <a href="/crud_incidencias" class="btn btn-primary">Volver</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>DATOS </h1>
-</body>
-</html>
-
--- resources/views/form_cliente.blade.php
+++ resources/views/form_cliente.blade.php
@@ -1,62 +1,65 @@
-{{-- @extends('layouts.app') --}}
-
-{{-- @section('content') --}}
-
-<div class="container">
-    <h1>FORM | INCIDENCIAS</h1>
-    <form action="{{ route('form_cliente.store') }}" method="post" enctype="multipart/form-data">
-        @csrf 
-        <input type="text" name="titulo_inc" class="form-control" placeholder="Titulo..." value="{{ old('titulo_inc') }}">
-
-        @error('titulo_inc')
-        <br>
-        <span>*{{ $message }}</span> 
-        <br>
-        <br>
-        @enderror
-        <input type="text" name="desc_inc" class="form-control" placeholder="Descripción..." value="{{ old('desc_inc') }}">
-        @error('desc_inc')
-        <br>
-        <span>*{{ $message }}</span> 
-        <br>
-        <br>
-        @enderror
-        <label for="">Fecha:</label>
-        <input type="date" name="fecha_inc" value="{{ old('fecha_inc') }}">
-        @error('fecha_inc')
-        <br>
-        <span>*{{ $message }}</span> 
-        <br>
-        <br>
-        @enderror
-        <br>
-        <label for="">Subcategoria:</label>
-        <select name="id_subcat">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formulario de Incidencias</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <style>
+    .center-content {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+    .card {
+      max-width: 500px;
+      width: 100%;
+    }
+  </style>
+</head>
+<body>
+  <div class="center-content">
+    <div class="card">
+      <div class="card-body">
+        <h1 class="card-title">FORM | INCIDENCIAS</h1>
+        <form action="{{ route('form_cliente.store') }}" method="post" enctype="multipart/form-data">
+          @csrf 
+          <input type="text" name="titulo_inc" class="form-control mb-3" placeholder="Titulo..." value="{{ old('titulo_inc') }}">
+          @error('titulo_inc')
+          <span class="text-danger">{{ $message }}</span>
+          @enderror
+          <input type="text" name="desc_inc" class="form-control mb-3" placeholder="Descripción..." value="{{ old('desc_inc') }}">
+          @error('desc_inc')
+          <span class="text-danger">{{ $message }}</span>
+          @enderror
+          <label for="">Fecha:</label>
+          <input type="date" name="fecha_inc" class="form-control mb-3" value="{{ old('fecha_inc') }}">
+          @error('fecha_inc')
+          <span class="text-danger">{{ $message }}</span>
+          @enderror
+          <label for="">Subcategoria:</label>
+          <select name="id_subcat" class="form-control mb-3">
             @foreach($subcategorias as $subcategoria)
                 <option value="{{ $subcategoria->id_categoria }}">{{ $subcategoria->nombre_sub_cat }}</option>
             @endforeach
-        </select>
-        @error('id_subcat')
-        <br>
-        <span>*{{ $message }}</span> 
-        <br>
-        @enderror
-        <br>
-        <label for="file">Añade una imagen:</label>
-        <input type="file" name="foto_inc" id="foto_inc">
-        @error('foto_inc')
-        <br>
-        <span>*{{ $message }}</span> 
-        <br>
-        @enderror
-        <br>
-        <!-- FALTAN LAS FK -->
-        <input type="hidden" name="id_estado" id="id_estado" value="1">
-        <input type="submit" value="Enviar" name="enviar">
-
-        <a href="/crud_incidencias" class="btn btn-primary">Volver</a>
-
-    </form>
-</div>
-
-{{-- @endsection --}}
+          </select>
+          @error('id_subcat')
+          <span class="text-danger">{{ $message }}</span>
+          @enderror
+          <label for="file">Añade una imagen:</label>
+          <input type="file" name="foto_inc" id="foto_inc" class="form-control-file mb-3">
+          @error('foto_inc')
+          <span class="text-danger">{{ $message }}</span>
+          @enderror
+          <input type="hidden" name="id_estado" id="id_estado" value="1">
+          <div class="mb-3">
+            <input type="submit" value="Enviar" name="enviar" class="btn btn-primary">
+            <a href="/crud_incidencias" class="btn btn-secondary">Volver</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
